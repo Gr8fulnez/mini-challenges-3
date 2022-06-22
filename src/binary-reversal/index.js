dfb7076a68ebd08ff1ad19d030b12b30467f03d7
New User <laptop@News-MacBook-Pro.local>
2022-06-22 02:22:00 -0700

    mini-challenge3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,23 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+
+  let strToNum = Number(value)
+    console.log(typeof(strToNum))
+    let t = strToNum.toString(2).split("");
+    console.log(t)
+    console.log(typeof(t))
+  let str_len = t.length;
+  for (let i = 0;i < 8 - str_len; i++){
+    t.unshift("0");
+  }
+  return parseInt(t.reverse().join(""),2).toString();
+
+}
+console.log(binaryReversal(47))
 
 module.exports = binaryReversal;
+
+
+
